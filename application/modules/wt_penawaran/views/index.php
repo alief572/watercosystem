341e82265e81446cbf6f18c892bfecbcb96de8f7
alief572 <146901652+alief572@users.noreply.github.com>
2025-04-25 10:28:35 +0700

    Update index.php

--- application/modules/wt_penawaran/views/index.php
+++ application/modules/wt_penawaran/views/index.php
@@ -24,7 +24,7 @@ $ENABLE_DELETE  = has_permission('Penawaran.Delete');
 	</div>
 	<!-- /.box-header -->
 	<!-- /.box-header -->
-	<div class="box-body">
+	<div class="box-body table-responsive">
 		<table id="example5" class="table table-bordered table-striped">
 			<thead>
 				<tr>
