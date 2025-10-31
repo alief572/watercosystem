88668c0f4fdcd4450a48b29b00566738b3b41aaf
alief572 <146901652+alief572@users.noreply.github.com>
2025-10-31 11:21:40 +0700

    Update base_url configuration in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -33,8 +33,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $root = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https://" : "http://");
 $root .= $_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-// $config['base_url'] = $root;
-$config['base_url'] = 'http://localhost:8080';
+$config['base_url'] = $root;
+// $config['base_url'] = 'http://localhost:8080';
 
 /*
 |--------------------------------------------------------------------------
